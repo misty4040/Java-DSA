87b7c4ae6ac7e17813f3f9610a71316bd598bc92
Misty Soni <mistysoni@Mistys-MacBook-Air.local>
2025-02-25 10:33:38 +0530

    Learning

---
+++ src/Polymorphism/Shapes.java
@@ -0,0 +1,13 @@
+package Polymorphism;
+
+public class Shapes {
+    void area(){
+        System.out.println("I am in shapes");
+    }
+
+//    early binding
+//    fnal void area(){
+//        System.out.println("I am in shapes");
+//    }
+
+}
---
+++ src/Polymorphism/Square.java
@@ -0,0 +1,8 @@
+package Polymorphism;
+
+public class Square extends Shapes {
+    void area(){
+        System.out.println("Area is sq of side");
+    }
+
+}
