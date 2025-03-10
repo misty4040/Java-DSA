cfde8becf0125a2ef3f2b57b1eb10b2a0999768f
misty4040 <mistysoni40@gmail.com>
2025-03-10 22:27:52 +0530

    difficulty:1047

---
+++ src/CodeChef/ChefAndHisStudents.java
@@ -0,0 +1,25 @@
+package CodeChef;
+import java.util.*;
+import java.lang.*;
+//import java.io.*;
+public class ChefAndHisStudents {
+
+
+        public static void main (String[] args) throws java.lang.Exception
+        {
+            // your code goes here
+            Scanner in=new Scanner(System.in);
+            int t=in.nextInt();
+            while(t-- >0){
+                String s=in.next();
+                int count=0;
+                for(int i=0;i<s.length()-1;i++){
+                    if(s.charAt(i) == '<' && s.charAt(i+1)== '>' ){
+                        count++;
+                    }
+                }
+                System.out.println(count);
+            }
+        }
+    }
+
