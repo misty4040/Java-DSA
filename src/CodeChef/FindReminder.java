43fb4234a338a8b9dd5d1fc9ce8300ea4c451467
misty4040 <mistysoni40@gmail.com>
2025-03-24 23:16:59 +0530

    learning

---
+++ src/CodeChef/FindReminder.java
@@ -0,0 +1,22 @@
+package CodeChef;
+import java.util.*;
+import java.lang.*;
+
+
+public class FindReminder {
+
+
+        public static void main (String[] args) throws java.lang.Exception
+        {
+            // your code goes here
+            Scanner in=new Scanner(System.in);
+            int t=in.nextInt();
+            while(t-- >0){
+                int a=in.nextInt();
+                int b=in.nextInt();
+                System.out.println(a%b);
+            }
+        }
+    }
+
+
