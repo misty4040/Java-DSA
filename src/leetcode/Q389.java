76ac5f62bbe465e7471fdcaaf460aae3bb8acb18
misty4040 <mistysoni40@gmail.com>
2025-04-06 23:40:29 +0530

    new learning

---
+++ src/leetcode/Q389.java
@@ -0,0 +1,28 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class Q389 {
+    public static void main(String[] args) {
+        String s = "a";
+        String t = "aa";
+        char res=findTheDifference(s, t);
+        System.out.println(res);
+    }
+
+    private static char findTheDifference(String s, String t) {
+        char[] ch1=s.toCharArray();
+        char[] ch2=t.toCharArray();
+
+        Arrays.sort(ch1);
+        Arrays.sort(ch2);
+
+        for(int i=0;i<ch1.length;i++){
+            if(ch1[i]!=ch2[i]){
+                return ch2[i];
+            }
+        }
+        return ch2[ch2.length-1];
+    }
+
+}
