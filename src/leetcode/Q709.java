81532b61e4a766a32d057368098086fadd1892f7
misty4040 <mistysoni40@gmail.com>
2025-04-17 21:38:58 +0530

    new learning

---
+++ src/leetcode/Q709.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+public class Q709 {
+    public static void main(String[] args) {
+        String s = "Hello";
+        String res=toLowerCase(s);
+        System.out.println(res);
+    }
+
+    private static String toLowerCase(String s) {
+        String var=s.toLowerCase();
+        return var;
+    }
+}
