21a7f1d34ad3ab93b74e50f5e124c865ea352112
misty4040 <mistysoni40@gmail.com>
2025-04-17 22:26:46 +0530

    new learning

--- src/leetcode/Q709.java
+++ src/leetcode/Q709.java
@@ -3,12 +3,24 @@ package leetcode;
 public class Q709 {
     public static void main(String[] args) {
         String s = "Hello";
-        String res=toLowerCase(s);
+        String res = toLowerCase(s);
         System.out.println(res);
     }
 
     private static String toLowerCase(String s) {
-        String var=s.toLowerCase();
-        return var;
+//        String var=s.toLowerCase();
+//        return var;
+        char[] ch = s.toCharArray();
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if(ch[i]>='A'&& ch[i]<='Z') {
+                char var = (char) ((char) ch[i] + 32);
+                res.append(var);
+            }else{
+                res.append(ch[i]);
+            }
+
+        }
+        return res.toString();
     }
 }
