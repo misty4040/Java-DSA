5dfae41deeb7f9e1ecd0cba983a1eee610901344
misty4040 <mistysoni40@gmail.com>
2025-05-13 22:56:25 +0530

    new learning

---
+++ src/leetcode/Q1967.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+public class Q1967 {
+    public static void main(String[] args) {
+        String[] patterns = {"a","abc","bc","d"};
+        String word = "abc";
+        System.out.println(numOfStrings(patterns,word));
+    }
+
+    private static int numOfStrings(String[] patterns, String word) {
+        int count=0;
+        for(int i=0;i< patterns.length;i++){
+            if(word.contains(patterns[i])){
+                count++;
+            }
+        }
+        return count;
+    }
+}
