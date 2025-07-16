16c4d73f2b8912552ae7d7beedeebd817d0f6f04
misty4040 <mistysoni40@gmail.com>
2025-07-17 00:22:47 +0530

    New Leetcode Question

---
+++ src/DSA/VariableSizeWindow.java
@@ -0,0 +1,23 @@
+package DSA;
+
+public class VariableSizeWindow {
+    public static void main(String[] args) {
+        int[] arr ={2,3,1,2,4,3};
+        System.out.println(findmin(arr,7));
+    }
+
+    private static int findmin(int[] arr, int target) {
+        int sum=0;
+        int start =0;
+        int mins = Integer.MAX_VALUE;
+        int end;
+        for(end=0;end<arr.length;end++){
+            sum+=arr[end];
+            while(sum>=target){
+                mins = Math.min(mins,(end-start)+1);
+                sum-= arr[start++];
+            }
+        }
+        return  mins;
+    }
+}
